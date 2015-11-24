5cffecc6fd851ad98f220378746f192cd03ab838
Siebe Jan <siebejan@hackerone.com>
2015-11-24 14:32:08 +0100

    move to classes

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,28 +1,6 @@
 class ExamplesController < ApplicationController
   before_action :authenticate_user!
 
-  class ResultsViewer
-    attr_reader :results
-
-    def initialize(results)
-      @results = results
-    end
-
-    def each_with_maybe_previous
-      previous = nil
-      results.each do |current|
-        if previous
-          query_change = previous.queries_count - current.queries_count
-        else
-          query_change = 0
-        end
-
-        yield(previous, current, query_change)
-        previous = current
-      end
-    end
-  end
-
   def show
     @example = example
     @results_viewer = ResultsViewer.new(results_for_this_example)
