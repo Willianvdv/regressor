d6a207fbb3768a54da8137269e1a0c7d2f74f3b4
Siebe Jan <siebejan@hackerone.com>
2015-11-30 10:39:49 +0100

    fixes and specs

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,6 @@
 class ResultsController < BackendController
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]  # TODO: check for API token
 
   def create
     render :ok, json: create_results
