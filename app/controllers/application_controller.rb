b204a9b503c9ec05cf9d053b53577127d2627b46
Siebe Jan <siebejan@hackerone.com>
2015-12-10 14:01:08 +0100

    remove csrf protection from api controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
-  def new_session_path(scope)
-    user_omniauth_authorize_path :github
-  end
-
   def serialize(things)
     ActiveModel::ArraySerializer.new things
   end
